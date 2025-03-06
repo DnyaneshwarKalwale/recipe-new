60081115835ba48dc7b598d52815a3b49c5f383d
Dnyaneshwar Balaji Kalwale <prembkalwale@gmail.com>
2025-03-07 03:30:44 +0530

    Removed node_modules from tracking

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const express = require('express');
 
-const app = express();
+
 
 app.use(cors()); // Allows all origins
 app.use(express.json());
